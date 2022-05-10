ab604b094a8ba01b8ff480f1c1d657ae7473136c
Chad Stabler <stablerpsn@gmail.com>
2022-05-10 14:25:41 -0500

    added extra cars to supercar array

--- data.js
+++ data.js
@@ -1,5 +1,7 @@
 export const superCars = [
     { id: '1', name: 'Jesko Absolut', manufacturer: 'Koenigsegg', topSpeed: '330+', image: './assets/jesko.jpg' },
     { id: '2', name: 'Divo', manufacturer: 'Bugatti', topSpeed: '236', image: './assets/divo.jpg' }, 
-    { id: '3', name: 'Huracan EVO', manufacturer: 'Lamborghini', topSpeed: '202', image: './assets/huracan.jpg' }
+    { id: '3', name: 'Huracan EVO', manufacturer: 'Lamborghini', topSpeed: '202', image: './assets/huracan.jpg' },
+    { id: '4', name: 'LaFerrari', manufacturer: 'Ferrari', topSpeed: '218', image: './assets/laferrari.jpg' },
+    { id: '5', name: 'Venom F5', manufacturer: 'Hennessey', topSpeed: '311', image: './assets/venom.jpg' }
 ];
